98b695b5df8e6d4c2036cdc553b93d93eac196b6
DavidSanwald <davidsanwald@users.noreply.github.com>
2017-04-25 01:21:33 +0200

    Yay

--- src/components/__tests__/StyledPad.test.js
+++ src/components/__tests__/StyledPad.test.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import StyledPad from '../StyledPad'
+import renderer from 'react-test-renderer'
 import { matcher, serializer } from 'jest-styled-components'
+import StyledPad from '../StyledPad'
 
 expect.extend(matcher)
 expect.addSnapshotSerializer(serializer)
 
+// foo.js
+module.exports = function() {
+  // some implementation;
+};
+
+// test.js
+jest.mock('lodash.sample'); // this happens automatically with automocking
+const sample = require('lodash.sample');
+
+// foo is a mock function
+sample.mockImplementation((array) => array[0]);
+const spyOnClick = jest.fn();
+
+
 it('renders without crashing', () => {
-  const wrapper = shallow(<StyledPad
-    state='playing' />)
-  expect(wrapper).toMatchStyledComponentsSnapshot()
+  const tree = renderer.create(<StyledPad
+    state='playing' onClick={spyOnClick}  />).toJSON()
+  expect(tree).toMatchStyledComponentsSnapshot()
 })
